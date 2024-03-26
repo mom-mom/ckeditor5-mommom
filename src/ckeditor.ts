35f8474d2426d59052306c7dceccd320a7870525
Jeonghan Joo <joo3436@gmail.com>
2024-03-26 19:31:50 +0900

    <INIT>: I know this is GPL

---
+++ src/ckeditor.ts
@@ -0,0 +1,136 @@
+/**
+ * @license Copyright (c) 2014-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Autosave } from '@ckeditor/ckeditor5-autosave';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontBackgroundColor, FontColor } from '@ckeditor/ckeditor5-font';
+import { Heading, Title } from '@ckeditor/ckeditor5-heading';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import {
+	AutoImage,
+	Image,
+	ImageCaption,
+	ImageInsert,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload
+} from '@ckeditor/ckeditor5-image';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { SelectAll } from '@ckeditor/ckeditor5-select-all';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { Undo } from '@ckeditor/ckeditor5-undo';
+import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Alignment,
+		AutoImage,
+		Autoformat,
+		Autosave,
+		BlockQuote,
+		Bold,
+		Essentials,
+		FontBackgroundColor,
+		FontColor,
+		Heading,
+		HorizontalLine,
+		Image,
+		ImageCaption,
+		ImageInsert,
+		ImageResize,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		Italic,
+		Link,
+		LinkImage,
+		List,
+		MediaEmbed,
+		Paragraph,
+		PasteFromOffice,
+		RemoveFormat,
+		SelectAll,
+		SimpleUploadAdapter,
+		Table,
+		TableToolbar,
+		TextTransformation,
+		Title,
+		Undo,
+		WordCount
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'selectAll',
+				'heading',
+				'|',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'outdent',
+				'indent',
+				'alignment',
+				'|',
+				'imageInsert',
+				'imageUpload',
+				'insertTable',
+				'mediaEmbed',
+				'blockQuote',
+				'undo',
+				'redo',
+				'-',
+				'fontColor',
+				'fontBackgroundColor',
+				'bold',
+				'italic',
+				'removeFormat',
+				'horizontalLine'
+			],
+			shouldNotGroupWhenFull: true
+		},
+		language: 'ko',
+		image: {
+			toolbar: [
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'imageStyle:inline',
+				'imageStyle:block',
+				'imageStyle:side',
+				'linkImage'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells'
+			]
+		}
+	};
+}
+
+export default Editor;
