98e250039e3625c36d92b8297125c6ce30239e6f
Dayoung Kim <dayoung0601@hanmail.net>
2025-12-10 15:22:54 +0900

    <FIX>: 0.1.16 import css file in simple/index.html

--- webpack.config.js
+++ webpack.config.js
@@ -84,9 +84,6 @@ module.exports = {
 				loader: 'postcss-loader',
 				options: {
 					postcssOptions: styles.getPostCssConfig( {
-						themeImporter: {
-							themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-						},
 						minify: true
 					} )
 				}
